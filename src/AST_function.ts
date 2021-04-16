3fbdbddbd7274b4826fb4852cb77ea5b6fdeda57
mierenmanz <christiaanmens@gmail.com>
2021-04-16 17:04:39 +0200

    optimize object

--- src/AST_function.ts
+++ src/AST_function.ts
@@ -1,46 +1,49 @@
 import type { Attributes, Node } from "./AST_types.ts";
 import { NodeType } from "./AST_types.ts";
 
-export function normalLine(text: string, attributes: Attributes): Node {
+export function normalLine(line: string): Node {
   return {
     type: NodeType.NormalLine,
     text: {
-      value: text,
-      attributes,
+      value: line,
+      attributes: getAttributes(line),
     },
+    includeNextLine: !(line.endsWith("  ") || line.endsWith("\\")),
   };
 }
 
 export function newLine(): Node {
   return {
     type: NodeType.NewLine,
+    includeNextLine: false,
   };
 }
 
-export function listNode(text: string, attributes: Attributes): Node {
+export function listNode(line: string): Node {
   return {
-    type: /^-\s+/.test(text) ? NodeType.UList : NodeType.SList,
+    type: /^-\s+/.test(line) ? NodeType.UList : NodeType.SList,
     text: {
-      value: text,
-      attributes,
+      value: line,
+      attributes: getAttributes(line),
     },
+    includeNextLine: !(line.endsWith("  ") || line.endsWith("\\")),
   };
 }
 
-export function headerNode(text: string, attributes: Attributes): Node {
+export function headerNode(line: string): Node {
   return {
     type: NodeType.Header,
-    header: `H${text.split(" ")[0].length}`,
+    header: `H${line.split(" ")[0].length}`,
     text: {
-      value: text,
-      attributes,
+      value: line,
+      attributes: getAttributes(line),
     },
+    includeNextLine: !(line.endsWith("  ") || line.endsWith("\\")),
   };
 }
 
 export function getAttributes(line: string): Attributes {
   return {
-    includeNextLine: !(line.endsWith("  ") || line.endsWith("\\")),
     imageOrLink: /\[(.*)\]\((.*)\)/.test(line),
     strike: /~~(.*)~~/.test(line),
     boldOrItalics: /\*(.*)\*|_(.*)_/.test(line),
